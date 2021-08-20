bd6b42c22432130fe71657a4c048ff9a0b586210
DevStar999 <wolverinerage99@gmail.com>
2021-08-20 11:53:21 +0530

    Created a custom toolbar replacing the default

--- app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.mdviewpagerandimageslideshowsectionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    private void initialise() {
+        toolbar = findViewById(R.id.tool_bar);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
+        setSupportActionBar(toolbar);
     }
 }
