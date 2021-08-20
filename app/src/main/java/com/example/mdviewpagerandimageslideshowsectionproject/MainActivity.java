c2acb116b06b1654d86eae3986adee6ea0570143
DevStar999 <wolverinerage99@gmail.com>
2021-08-20 12:48:20 +0530

    Implemented a basic slideshow

--- app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.mdviewpagerandimageslideshowsectionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private ViewPager viewPager;
+    private SlideShowAdapter slideShowAdapter;
 
     private void initialise() {
         toolbar = findViewById(R.id.tool_bar);
+        viewPager = findViewById(R.id.view_pager);
+        slideShowAdapter = new SlideShowAdapter(this);
     }
 
     @Override
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initialise();
+
         setSupportActionBar(toolbar);
+
+        viewPager.setAdapter(slideShowAdapter);
     }
 }
