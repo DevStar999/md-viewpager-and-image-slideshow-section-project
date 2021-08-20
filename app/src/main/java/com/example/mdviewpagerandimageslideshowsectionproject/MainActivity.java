6d6baebc1170fc9263fa5b27a9d45fb5bdd4282d
DevStar999 <wolverinerage99@gmail.com>
2021-08-20 14:02:50 +0530

    Implemented timers for the automatic scrolling of images after a set interval

--- app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdviewpagerandimageslideshowsectionproject/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.mdviewpagerandimageslideshowsectionproject;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -14,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
     private SlideShowAdapter slideShowAdapter;
     private CircleIndicator circleIndicator;
 
+    // Timer related attributes
+    private Handler handler;
+    private Timer timer;
+
     private void initialise() {
         toolbar = findViewById(R.id.tool_bar);
         viewPager = findViewById(R.id.view_pager);
         slideShowAdapter = new SlideShowAdapter(this);
         circleIndicator = findViewById(R.id.circle_indicator);
+
+        // Initialising Timer related attributes
+        handler = new Handler();
+        timer = new Timer();
     }
 
     @Override
@@ -33,5 +46,19 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(slideShowAdapter);
 
         circleIndicator.setViewPager(viewPager);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        viewPager.setCurrentItem(
+                                (viewPager.getCurrentItem()+1)%slideShowAdapter.getCount(),
+                                true);
+                    }
+                });
+            }
+        }, 2000, 2000);
     }
 }
